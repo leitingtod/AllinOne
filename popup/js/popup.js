05554255a5de20c4aa54f899e0c5537136273655
leiting <leitingtod@126.com>
2018-11-29 13:59:26 +0800

    update popup icons, cleanup code

--- popup/js/popup.js
+++ popup/js/popup.js
@@ -2,8 +2,10 @@ chrome.management.getAll(function (result) {
     let extensions = {};
     result.map(function (extension) {
         extension.name = extension.name.split('：')[0];
-        extension.name = extension.name.split('-')[0];
-        if (extension.name.indexOf('多合一扩展') === -1) {
+        extension.name = extension.name.split('-')[0].substring(0, 11);
+
+        if ((extension.icons != null && extension.icons.length > 0) &&
+            extension.name.indexOf('多合一扩展') === -1) {
             extensions[extension.id] = extension;
         }
     });
@@ -19,9 +21,9 @@ chrome.management.getAll(function (result) {
                 extensions: extensions,
                 statics: enabled.length + '/' + result.length,
                 visits: {
-                    'history': 'history',
+                    'history': 'watch_later',
                     'bookmarks': 'bookmark',
-                    'downloads': 'vertical_align_bottom',
+                    'downloads': 'get_app',
                     'extensions': 'extension',
                     'settings': 'settings'
                 }
@@ -37,25 +39,30 @@ chrome.management.getAll(function (result) {
                 chrome.tabs.getAllInWindow(null, function (tabs) {
                     let prefix = 'chrome://';
                     for (var i = 0; i < tabs.length; i++) {
-                        console.log(tabs[i].url.replace(prefix, '').replace('/', ''), target);
                         if (tabs[i].url.replace(prefix, '').replace('/', '') === target) {
-                            chrome.tabs.update(tabs[i].id, {selected: true});
+                            chrome.tabs.update(tabs[i].id, {
+                                selected: true
+                            });
                             window.close();
                             return;
                         }
                     }
-                    chrome.tabs.create({url: prefix + target, selected: true});
+                    chrome.tabs.create({
+                        url: prefix + target,
+                        selected: true
+                    });
                     window.close();
                 });
             },
-            hover: function() {
+            hover: function () {
 
             },
             status: function (item) {
-                return {'status': item.enabled ? 'on' : 'off', 'badge': item.enabled ? 'success' : 'secondary'};
+                return {
+                    'status': item.enabled ? 'on' : 'off',
+                    'badge': item.enabled ? 'success' : 'secondary'
+                };
             }
         }
     });
 });
-
-
